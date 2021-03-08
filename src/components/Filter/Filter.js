88e528fd7818ed0d317032cacbfcf85ea4b076b2
Larisa Antsifrova <larisa.antsifrova@gmail.com>
2021-03-08 11:53:58 +0200

    Added basic styles for Filter.

--- src/components/Filter/Filter.js
+++ src/components/Filter/Filter.js
@@ -1,18 +1,22 @@
 import React from 'react';
 
+import styles from './Filter.module.css';
+
 const Filter = ({ filterValue, filterUpdate }) => {
   return (
-    <label>
-      Find contacts by name
-      <input
-        type="text"
-        name="filter"
-        value={filterValue}
-        onChange={filterUpdate}
-        autoComplete="off"
-        required
-      />
-    </label>
+    <div className={styles.filter}>
+      <label>
+        Find contacts by name
+        <input
+          type="text"
+          name="filter"
+          value={filterValue}
+          onChange={filterUpdate}
+          autoComplete="off"
+          required
+        />
+      </label>
+    </div>
   );
 };
 
