0e69a32f7aecda1602b5c84cfd65a706c85775aa
Larisa Antsifrova <larisa.antsifrova@gmail.com>
2021-03-08 11:44:36 +0200

    Added basic styles for ContactList component.

--- src/components/ContactList/ContactList.js
+++ src/components/ContactList/ContactList.js
@@ -24,7 +24,7 @@ const ContactList = ({ filtered, onDeleteContact }) => {
           </li>
         ))
       ) : (
-        <li>No contact found</li>
+        <li className={styles.notification}>No contact found</li>
       )}
     </ul>
   );
